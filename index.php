438ef45b6ad5ac58d5b3cd2e45518150a5a5fa9b
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-02 12:00:27 +0200

    added html tree inside php  page

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MY PHP Learning</title>
+</head>
+<body>
+    <h1>PHP Teting grounds</h1>
+
+    <?php
 echo "Hello World";
+
+$a = 5;
+echo "<p>My Lorem" . $a . "</p>";
+?>
+<hr> 
+
+<?php
+echo "more php" . $a;
+
+
+</body>
+</html>
+
+
+
+
+
