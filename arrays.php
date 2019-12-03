497c29e6d40c2bb9457608ee4559e1903e51d227
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-03 11:44:04 +0200

    for each loop

--- arrays.php
+++ arrays.php
@@ -9,3 +9,22 @@ $supermarkets = [
     "lowend" => "Lidl",
 ];
 var_dump($supermarkets);
+
+echo "<hr>";
+array_push($cars, 'BMW', 'VW');
+var_dump($cars);
+echo "<hr>";
+echo $cars[2];
+foreach ($cars as $car) {
+    echo "This is my auto:" . $car . "<br>";
+}
+echo "<ul>";
+foreach ($cars as $car) {
+    echo "<li>This is my auto:" . $car . "</li>";
+}
+echo "</ul>";
+echo "<ol>";
+for ($i = 1; $i < sizeof($cars); $i++) {
+    echo "<li>Car Number $i is $cars[$i]</li>";
+}
+echo "</ol>";
