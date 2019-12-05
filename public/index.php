5eeb7824a2fe11ee57843ddb106ad7b7c3bb9e06
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-05 12:45:48 +0200

    reading boolean truth

--- public/index.php
+++ public/index.php
@@ -7,14 +7,19 @@ try {
     $conn = new PDO("mysql:host=$SERVER;dbname=$DB", USER, PW);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected successfully <br>";
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
 
+    $stmt = $conn->prepare("SELECT * FROM tracks");
+    $stmt->execute();
 
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+var_dump($result);
 
 
 
