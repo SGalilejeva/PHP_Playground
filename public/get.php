ba353ab2dd878431de3536377bc07061736fbdec
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-06 10:25:47 +0200

    added get.php

---
+++ public/get.php
@@ -0,0 +1,9 @@
+
+<form action="get.php" method="get">
+    <input type="text" name="myname" placeholder="Enter My Name" required>
+    <input type="text" name="lastname" placeholder="Enter My Last Name" required>
+    <input type="date" name="date" id="datepicker">
+    <button type="submit" name="submitBtn" value="33">Submit</button>
+
+
+</form>
