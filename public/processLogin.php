0ae106cb250d38adecc6d89d93f3b0e25fb96168
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-10 10:34:10 +0200

    added processLogin and processRegister php

---
+++ public/processLogin.php
@@ -0,0 +1,15 @@
+<?php
+require_once "../src/db.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // prepare and bind
+    $stmt = $conn->prepare("SELECT hash FROM users 
+    WHERE (username = :username)");
+    $stmt->bindParam(':username', $username);
+
+    $stmt->execute();
+
+}
