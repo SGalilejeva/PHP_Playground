3f2ce7f112c06851662fc5612a2896f6d5046fc9
Sandra Galilejeva <sandra.galilejeva@gmail.com>
2019-12-11 22:34:42 +0200

    finished print Task php code

--- public/index.php
+++ public/index.php
@@ -5,52 +5,51 @@ require_once '../public/config.php';
 require_once '../templates/head.php';
 require_once '../src/login.php';
 require_once '../src/printTask.php';
-
 require_once '../src/addTask.php';
 
-try {
-    $conn = new PDO("mysql:host=$SERVER;dbname=$DB", USER, PW);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully <br>";
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-// prepared a statement and executed it 
-    $stmt = $conn->prepare("SELECT * FROM tasks");
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $isFetchModeSet = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-var_dump($isFetchModeSet);
-// we store results in memory 
-$allRows = $stmt->fetchAll();
-// foreach ($allRows as $key => $value) {
-//     echo "<hr>";
-//     echo "KEY: ";
-//     var_dump($key);
-//     echo "<br>";
-//     var_dump($value); 
-// }
-
-
-// $columnsPrinted = false;
-// foreach ($allRows as $row) {
-//     if (!$columnsPrinted) {
-//         foreach ($row as $key => $value) {
-//             echo "KEY: $key <br>";
-//         }
-//         $columnsPrinted = true; 
+// try {
+//     $conn = new PDO("mysql:host=$SERVER;dbname=$DB", USER, PW);
+//     // set the PDO error mode to exception
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected successfully <br>";
 //     }
-    
-//     echo "<div>";
-//     echo "<span> Task: " . $row["task"] . "</span>";
-//     echo "<span>Location: " . $row["location"] . "</span>";
-//     echo "<hr>";
-//     echo "</div>";
+// catch(PDOException $e)
+//     {
+//     echo "Connection failed: " . $e->getMessage();
+//     }
+// // prepared a statement and executed it 
+//     $stmt = $conn->prepare("SELECT * FROM tasks");
+//     $stmt->execute();
 
-// }
+//     // set the resulting array to associative
+//     $isFetchModeSet = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+// var_dump($isFetchModeSet);
+// // we store results in memory 
+// $allRows = $stmt->fetchAll();
+// // foreach ($allRows as $key => $value) {
+// //     echo "<hr>";
+// //     echo "KEY: ";
+// //     var_dump($key);
+// //     echo "<br>";
+// //     var_dump($value); 
+// // }
+
+
+// // $columnsPrinted = false;
+// // foreach ($allRows as $row) {
+// //     if (!$columnsPrinted) {
+// //         foreach ($row as $key => $value) {
+// //             echo "KEY: $key <br>";
+// //         }
+// //         $columnsPrinted = true; 
+// //     }
+    
+// //     echo "<div>";
+// //     echo "<span> Task: " . $row["task"] . "</span>";
+// //     echo "<span>Location: " . $row["location"] . "</span>";
+// //     echo "<hr>";
+// //     echo "</div>";
+
+// // }
 
 
--- src/printTask.php
+++ src/printTask.php
@@ -11,13 +11,16 @@ $columnsPrinted = false;
 foreach ($allRows as $row) {
     if (!$columnsPrinted) {
         foreach ($row as $key => $value) {
-            echo "KEY: $key <br>";
+            echo "<span class='column-name'> $key </span>";
         }
         $columnsPrinted = true;
     }
     echo "<div>";
-    echo "<span>Task: " . $row["task"] . "</span>";
-    echo "<span>Location: " . $row["location"] . "</span>";
+    // echo "<span>Task: " . $row["task"] . "</span>";
+    // echo "<span>Location: " . $row["location"] . "</span>";
+    foreach ($row as $key => $value) {
+        echo "<span class='value-cell'>$value </span>";
+    }
     echo "<hr>";
     echo "</div>";
 }
